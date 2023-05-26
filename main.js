16a040e0cf8e0598bedfee36d1dfa85c68911f6e
Eli Wawersik <ewawersik@gmail.com>
2023-05-26 09:33:59 -0400

    Create outcome ladder for playRound

--- main.js
+++ main.js
@@ -2,10 +2,50 @@ function getComputerChoice() {
     let choice = Math.floor(Math.random()*3)
     switch(choice) {
         case 0:
-            return rock;
+            return 'rock';
         case 1:
-            return paper;
+            return 'paper';
         case 2:
-            return scissors;
+            return 'scissors';
     }
 }
+
+function playRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection.toLowerCase();
+    if (playerSelection == 'rock') {
+        if (computerSelection == 'rock') {
+            return "It's a tie!"
+        }
+        if (computerSelection == 'paper') {
+            return "You lose! Rock loses to paper"
+        }
+        if (computerSelection == 'scissors') {
+            return "You win! Rock beats scissors"
+        }
+    } else if (playerSelection == 'paper') {
+        if (computerSelection == 'paper') {
+            return "It's a tie!"
+        }
+        if (computerSelection == 'scissors') {
+            return "You lose! Paper loses to scissors"
+        }
+        if (computerSelection == 'rock') {
+            return "You win! Paper beats rock"
+        }
+    } else if (playerSelection == 'scissors') {
+        if (computerSelection == 'scissors') {
+            return "It's a tie!"
+        }
+        if (computerSelection == 'rock') {
+            return "You lose! Scissors loses to rock"
+        }
+        if (computerSelection == 'paper') {
+            return "You win! Scissors beats paper"
+        }
+    } else {
+        return "Please enter rock, paper, or scissors"
+    }
+}
+
+const playerSelection = 'rock';
+const computerSelection = getComputerChoice();
